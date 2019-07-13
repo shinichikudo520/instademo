38a10e6f70e9b8e950a162b8dce56b96208717e8
erhuyeah <erhuyeah@protonmail.com>
2019-07-14 07:25:48 +0800

    201909140725

--- Insta/views.py
+++ Insta/views.py
@@ -3,3 +3,4 @@ from django.views.generic import TemplateView
 # Create your views here.
 class HelloDjango(TemplateView):
     template_name = 'home.html'
+    
---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/hubiyu/哦耶/IT资料库/课件/Django/code/env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
